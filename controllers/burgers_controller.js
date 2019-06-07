cae4e807880cfcbffde3f8569a12a46d9c842de2
dolaku <dolaku@gmail.com>
2019-06-06 22:36:58 -0400

    Added Post api route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -13,6 +13,21 @@ router.get('/', (req, res) => {
     });
 });
 
+router.post('/api/burgers', (req, res) => {
+    burger.insertOne([
+        'burger_name', 'devoured' 
+    ],[
+        req.body.name, req.body.devoured
+    ], (result) => {
+        res.json({ id: result.id });
+    });
+});
+
+router.put('/api/burgers/:id', (req, res) => {
+    let condition = req.params.id;
+    console.log(condition);
+});
+
 
 // Export routes for server.js to use.
 module.exports = router;
